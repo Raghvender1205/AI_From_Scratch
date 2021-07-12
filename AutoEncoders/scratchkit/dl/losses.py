fd9adfebe9f63d1948f6ef9cd59ce81f7843ccac
Raghvender <raghvender1205@gmail.com>
2021-07-12 22:21:21 +0530

    Losses

---
+++ AutoEncoders/scratchkit/dl/losses.py
@@ -0,0 +1,30 @@
+import numpy as np
+from scratchkit.utils import accuracy_score
+from scratchkit.dl.activations import Sigmoid
+
+class Loss(object):
+    def loss(self, y_true, y_pred):
+        return NotImplementedError()
+
+    def gradient(self, y, y_pred):
+        raise NotImplementedError()
+
+    def acc(self, y, y_pred):
+        return 0
+
+class SquareLoss(Loss):
+    def __init__(self): pass
+
+    def loss(self, y, y_pred):
+        return 0.5 * np.power((y - y_pred), 2)
+
+    def gradient(self, y, y_pred):
+        return -(y - y_pred)
+
+class CrossEntropy(Loss):
+    def __init__(self): pass
+
+    def loss(self, y, p):
+        # Avoid division by zero
+        p = np.clip(p, 1e-15, 1 - 1e-15)
+        return - y * np.log(p) - (1 - y) * np.log(1 - p)
