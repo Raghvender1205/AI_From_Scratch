199736ab9b85425290653942acd9599dc6a54fe7
Raghvender <raghvender1205@gmail.com>
2021-10-27 03:30:06 +0530

    Fixed Some Internal Errors

--- Convolution Neural Network Essentials/CNN From Scratch/Softmax.py
+++ Convolution Neural Network Essentials/CNN From Scratch/Softmax.py
@@ -1,73 +1,70 @@
 import numpy as np
 
-# A Fully connected layer with softmax activation
+
 class Softmax:
+    # A standard fully-connected layer with softmax activation.
+
     def __init__(self, input_len, nodes):
-        # Divide the input_len to reduce variance of the initial values
+        # We divide by input_len to reduce the variance of our initial values
         self.weights = np.random.randn(input_len, nodes) / input_len
         self.biases = np.zeros(nodes)
 
-    def forward(self, x):
-        """
-        Perform Forward Pass for this layer using x.
-        Input:
-            Any Array with Any dimension
-        Return:
-            1D NumPy array containing respective probability values
-        """ 
-        self.last_input_shape = x.shape
+    def forward(self, input):
+        '''
+        Performs a forward pass of the softmax layer using the given input.
+        Returns a 1d numpy array containing the respective probability values.
+        - input can be any array with any dimensions.
+        '''
+        self.last_input_shape = input.shape
 
-        x = x.flatten()
-        self.last_input = x
+        input = input.flatten()
+        self.last_input = input
 
         input_len, nodes = self.weights.shape
-        totals = np.dot(x, self.weights) + self.biases # f(x) = W.x + b
+
+        totals = np.dot(input, self.weights) + self.biases
         self.last_totals = totals
 
         exp = np.exp(totals)
         return exp / np.sum(exp, axis=0)
 
-
-    def backward(self, dL_d_out, lr):
-        """
-        Perform Backpropgation for Softmax Layer.
-        Input:
-            - dL_d_out -> Loss gradient for the layer's outputs.
-            - lr -> Learning Rate
-
-        Return:
-            Loss gradient for this layer's input
-        """
-        # As we know only 1 element of dL_d_out will be nonzero 
-        for i, gradient in enumerate(dL_d_out):
+    def backward(self, d_L_d_out, learn_rate):
+        '''
+        Performs a backward pass of the softmax layer.
+        Returns the loss gradient for this layer's inputs.
+        - d_L_d_out is the loss gradient for this layer's outputs.
+        - learn_rate is a float.
+        '''
+        # We know only 1 element of d_L_d_out will be nonzero
+        for i, gradient in enumerate(d_L_d_out):
             if gradient == 0:
                 continue
 
-        # e ^ totals
-        t_exp = np.exp(self.last_totals)
+            # e^totals
+            t_exp = np.exp(self.last_totals)
 
-        # Sum of all e ^ totals
-        S = np.sum(t_exp)
+            # Sum of all e^totals
+            S = np.sum(t_exp)
 
-        # Gradient of out[i] against totals
-        d_out_d_t = -t_exp[i] * t_exp / (S ** 2)
-        d_out_d_t[i] = t_exp[i] * (S - t_exp[i]) / (S ** 2)
+            # Gradients of out[i] against totals
+            d_out_d_t = -t_exp[i] * t_exp / (S ** 2)
+            d_out_d_t[i] = t_exp[i] * (S - t_exp[i]) / (S ** 2)
 
-        # Gradients of totals against weights / biases / input
-        d_t_d_w = self.last_input
-        d_t_d_b = 1
-        d_t_d_inputs = self.weights
+            # Gradients of totals against weights/biases/input
+            d_t_d_w = self.last_input
+            d_t_d_b = 1
+            d_t_d_inputs = self.weights
 
-        # Gradients of loss against totals
-        d_L_d_t = gradient * d_out_d_t
+            # Gradients of loss against totals
+            d_L_d_t = gradient * d_out_d_t
 
-        # Gradients of loss against weights / biases / input
-        d_L_d_w = d_t_d_w[np.newaxis].T @ d_L_d_t[np.newaxis]
-        d_L_d_b = d_L_d_t * d_t_d_b
-        dL_d_inputs = d_t_d_inputs @ d_L_d_t
+            # Gradients of loss against weights/biases/input
+            d_L_d_w = d_t_d_w[np.newaxis].T @ d_L_d_t[np.newaxis]
+            d_L_d_b = d_L_d_t * d_t_d_b
+            d_L_d_inputs = d_t_d_inputs @ d_L_d_t
 
-        # Update weights / biases
-        self.weights -= lr * d_L_d_w
-        self.biases -= lr * d_L_d_b
+            # Update weights / biases
+            self.weights -= learn_rate * d_L_d_w
+            self.biases -= learn_rate * d_L_d_b
 
-        return dL_d_inputs.reshape(self.last_input_shape)
+            return d_L_d_inputs.reshape(self.last_input_shape)
