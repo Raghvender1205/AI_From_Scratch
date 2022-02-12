eaf21d841241fc82e2472d3571ad4da0599dfec8
Raghvender <raghvender1205@gmail.com>
2022-02-13 03:02:14 +0530

    TimesTracker

---
+++ Optimizers/Gradient-Free-Optimizers/gradient_free_optimizers/times_tracker.py
@@ -0,0 +1,27 @@
+import time
+
+
+class TimeTracker:
+    def __init__(self):
+        super().__init__()
+
+        self.eval_times = []
+        self.iter_times = []
+    
+    def eval_time(func):
+        def wrapper(self, *args, **kwargs):
+            t = time.time()
+            res = func(self, *args, **kwargs)
+            self.eval_times.append(time.time() - t)
+            return res
+
+        return wrapper
+    
+    def iter_time(func):
+        def wrapper(self, *args, **kwargs):
+            t = time.time()
+            res = func(self, *args, **kwargs)
+            self.iter_times.append(time.time() - t)
+            return res
+
+        return wrapper
