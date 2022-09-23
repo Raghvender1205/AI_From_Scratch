209527d9538f357e4ae08d0f1bd38fbe7164e5da
Raghvender <raghvender1205@gmail.com>
2022-09-23 23:22:38 +0530

    ModifiedResNet, ModifiedLayerNorm and QuickGELU added

--- Text2Image/CLIP/model.py
+++ Text2Image/CLIP/model.py
@@ -82,3 +82,84 @@ class AttentionPool2d(nn.Module):
         )
 
         return x.squeeze(0)
+
+class ModifiedResNet(nn.Module):
+    """
+    This ResNet has some modifications in the Architecture
+    - There are 3 "stem" convolutions as opposed to 1, with an AvgPool2d instead of MaxPool2d
+    - Performs anti-aliasing strided Conv2d, where AvgPool2d is prepended to Conv2d with stride > 1
+    - Final Pooling Layer is a QKV Attention instead of AvgPool2d
+    """
+
+    def __init__(self, layers, output_dim, heads, input_resolution=224, width=64):
+        super().__init__()
+        self.output_dim = output_dim
+        self.input_resolution = input_resolution
+
+        # 3 Layer Stem Convolutions
+        self.conv1 = nn.Conv2d(3, width // 2, kernel_size=3,
+                               stride=2, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(width // 2)
+        self.relu1 = nn.ReLU(inplace=True)
+
+        self.conv2 = nn.Conv2d(width // 2, width // 2,
+                               kernel_size=3, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(width // 2)
+        self.relu2 = nn.ReLU(inplace=True)
+
+        self.conv3 = nn.Conv2d(
+            width // 2, width, kernel_size=3, padding=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(width)
+        self.relu3 = nn.ReLU(inplace=True)
+        self.avgpool = nn.AvgPool2d(2)
+
+        # Residual Layers
+        self._inplanes = width
+        self.layer1 = self._make_layer(width, layers[0]) 
+        self.layer2 = self._make_layer(width * 2, layers[1], stride=2)
+        self.layer3 = self._make_layer(width * 4, layers[2], stride=2)
+        self.layer4 = self._make_layer(width * 8, layers[3], stride=2)
+
+        embed_dim = width * 32 # ResNet feature dimension
+        self.attnpool = AttentionPool2d(input_resolution // 32, embed_dim, heads, output_dim)
+
+    def _make_layer(self, planes, blocks, stride=1):
+        layers = [BottleNeck(self._inplanes, planes, stride)]
+
+        self._inplanes = planes * BottleNeck.expansion
+        for _ in range(1, blocks):
+            layers.append(BottleNeck(self._inplanes, planes))
+        
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        def stem(x):
+            x = self.relu1(self.bn1(self.conv1(x)))
+            x = self.relu2(self.bn2(self.conv2(x)))
+            x = self.relu3(self.bn3(self.conv3(x)))
+
+            return x
+
+        x = x.dtype(self.conv1.weight.dtype)
+        x = stem(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.avgpool(x)
+    
+        return x
+
+
+class LayerNorm(nn.Module):
+    """ A Subclass of nn.LayerNorm to handle FP16 """
+    def forward(self, x: torch.Tensor):
+        orig_type = x.dtype
+        ret = super().forward(x.type(torch.float32))
+
+        return ret.type(orig_type)
+    
+class QuickGELU(nn.Module):
+    """ Fast version of nn.GELU activation """
+    def forward(self, x: torch.Tensor):
+        x * F.sigmoid(1.702 * x)
