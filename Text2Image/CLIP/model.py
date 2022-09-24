1c277bdb72132d8878ec854ac9e1e9ba251df375
Raghvender <raghvender1205@gmail.com>
2022-09-24 15:40:02 +0530

    ResidualAttentionBlock and Transformer Added

--- Text2Image/CLIP/model.py
+++ Text2Image/CLIP/model.py
@@ -163,3 +163,40 @@ class QuickGELU(nn.Module):
     """ Fast version of nn.GELU activation """
     def forward(self, x: torch.Tensor):
         x * F.sigmoid(1.702 * x)
+
+
+class ResidualAttentionBlock(nn.Module):
+    def __init__(self, d_model: int, n_head: int, attn_mask: torch.Tensor = None):
+        super().__init__()
+
+        self.attn = nn.MultiheadAttention(d_model, n_head)
+        self.ln1 = LayerNorm(d_model)
+        self.mlp = nn.Sequential(OrderedDict([
+            ("c_fc", nn.Linear(d_model, d_model * 4)),
+            ("gelu", QuickGELU()),
+            ("c_proj", nn.Linear(d_model * 4, d_model))
+        ]))
+        self.ln2 = LayerNorm(d_model)
+        self.attn_mask = attn_mask
+    
+    def attention(self, x: torch.Tensor):
+        self.attn_mask = self.attn_mask.to(dtype=x.dtype, device=x.device) if self.attn_mask is not None else None
+
+        return self.attn(x, x, x, need_weights=False, attn_mask=self.attn_mask)[0]
+
+    def forward(self, x: torch.Tensor):
+        x = x + self.attention(self.ln1(x))
+        x = x + self.mlp(self.ln2(x))
+
+        return x
+
+
+class Transformer(nn.Module):
+    def __init__(self, width: int, layers: int, heads: int, attn_mask: torch.Tensor = None):
+        super().__init__()
+        self.width = width
+        self.layers = layers
+        self.resblocks = nn.Sequential(*[ResidualAttentionBlock(width, heads, attn_mask) for _ in range(layers)])
+
+    def forward(self, x: torch.Tensor):
+        return self.resblocks(x)
