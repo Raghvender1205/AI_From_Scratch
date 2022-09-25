0ccd2ed0e9acb5bf712395cd4855effed3bbec5a
Raghvender <raghvender1205@gmail.com>
2022-09-25 16:25:23 +0530

    Test CLIP

---
+++ Text2Image/CLIP/test.py
@@ -0,0 +1,58 @@
+import clip
+import numpy as np
+import os
+import skimage
+import IPython.display
+import matplotlib.pyplot as plt
+from PIL import Image
+import torch
+
+print(clip.available_models())
+# ['RN50', 'RN101', 'RN50x4', 'RN50x16', 'RN50x64', 'ViT-B/32', 'ViT-B/16', 'ViT-L/14', 'ViT-L/14@336px']
+model, preprocess = clip.load('ViT-B/32')
+model.cuda().eval()
+input_res = model.visual.input_res
+context_length = model.context_length
+vocab_size = model.vocab_size
+
+print("Model parameters:", f"{np.sum([int(np.prod(p.shape)) for p in model.parameters()]):,}")
+print("Input resolution:", input_res)
+print("Context length:", context_length)
+print("Vocab size:", vocab_size)
+
+
+# images in skimage to use and their textual descriptions
+descriptions = {
+    "page": "a page of text about segmentation",
+    "chelsea": "a facial photo of a tabby cat",
+    "astronaut": "a portrait of an astronaut with the American flag",
+    "rocket": "a rocket standing on a launchpad",
+    "motorcycle_right": "a red motorcycle standing in a garage",
+    "camera": "a person looking at a camera on a tripod",
+    "horse": "a black-and-white silhouette of a horse",
+    "coffee": "a cup of coffee on a saucer"
+}
+
+original_images = []
+images = []
+texts = []
+plt.figure(figsize=(16, 5))
+
+for filename in [filename for filename in os.listdir(skimage.data_dir) if filename.endswith(".png") or filename.endswith(".jpg")]:
+    name = os.path.splitext(filename)[0]
+    if name not in descriptions:
+        continue
+
+    image = Image.open(os.path.join(skimage.data_dir, filename)).convert("RGB")
+
+    plt.subplot(2, 4, len(images) + 1)
+    plt.imshow(image)
+    plt.title(f"{filename}\n{descriptions[name]}")
+    plt.xticks([])
+    plt.yticks([])
+
+    original_images.append(image)
+    images.append(preprocess(image))
+    texts.append(descriptions[name])
+
+plt.tight_layout()
