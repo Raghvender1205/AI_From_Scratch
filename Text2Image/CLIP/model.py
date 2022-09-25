5118ce11444b2129851340a43dd9aed7e4512b11
Raghvender <raghvender1205@gmail.com>
2022-09-25 15:41:44 +0530

    ViT and CLIP model Added

--- Text2Image/CLIP/model.py
+++ Text2Image/CLIP/model.py
@@ -200,3 +200,220 @@ class Transformer(nn.Module):
 
     def forward(self, x: torch.Tensor):
         return self.resblocks(x)
+    
+class ViT(nn.Module):
+    def __init__(self, input_res: int, patch_size: int, width: int, layers: int, heads: int, output_dim: int):
+        super().__init__()
+        self.input_res = input_res
+        self.output_dim = output_dim
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=width, kernel_size=patch_size, stride=patch_size, bias=False)
+
+        scale = width ** -0.5
+        self.class_emb = nn.Parameter(scale * torch.randn(width))
+        self.pos_emb = nn.Parameter(scale * torch.randn((input_res // patch_size) ** 2 + 1, width))
+        self.ln_pre = LayerNorm(width)
+
+        self.transformer = Transformer(width, layers, heads)
+        self.ln_post = LayerNorm(width)
+        self.proj = nn.Parameter(scale * torch.randn(width, output_dim))
+    
+    def forward(self, x: torch.Tensor):
+        x = self.conv1(x) # [*, width, grid, grid]
+        x = x.reshape(x.shape[0], x.shape[1], -1) # [*, width, grid ** 2]
+        x = x.permute(0, 2, 1) # [*, grid**2, width]
+        x = torch.cat([self.class_emb.to(x.dtype) + torch.zeros(x.shape[0], 1, x.shape[-1], dtype=x.dtype, device=x.device), x], dim=1)
+        x = x + self.pos_emb.to(x.dtype)    
+        x = self.ln_pre(x)
+
+        x = x.permute(1, 0, 2) # NLD -> LND
+        x = self.transformer(x)
+        x = x.permute(1, 0, 2) # LND -> NLD
+
+        x = self.ln_post(x[:, 0, :])
+        if self.proj is not None:
+            x = x @ self.proj
+
+        return x
+
+class CLIP(nn.Module):
+    def __init__(self, embed_dim: int, image_res: int, vision_layers: Union[Tuple[int, int, int, int], int],
+                 vision_width: int, vision_patch_size: int, context_length: int, vocab_size: int, 
+                 transformer_W: int, transformer_heads: int, transformer_layers: int):
+        super().__init__()
+
+        self.context_length = context_length
+        if isinstance(vision_layers, (tuple, list)):
+            vision_heads = vision_width * 32 // 64
+            self.visual = ModifiedResNet(
+                layers = vision_layers,
+                output_dim = embed_dim,
+                heads = vision_heads,
+                input_resolution = image_res,
+                width = vision_width
+            )
+        else:
+            vision_heads = vision_width // 64
+            self.visual = ViT(
+                input_res = image_res,
+                patch_size = vision_patch_size,
+                width = vision_width,
+                layers = vision_layers,
+                heads = vision_heads,
+                output_dim = embed_dim
+            )
+        
+        self.transformer = Transformer(
+            width = transformer_W,
+            layers = transformer_layers,
+            heads = transformer_heads,
+            attn_mask = self.build_attn_mask()
+        )
+
+        self.vocab_size = vocab_size
+        self.token_emb = nn.Embedding(vocab_size, transformer_W)
+        self.pos_emb = nn.Parameter(torch.empty(self.context_length, transformer_W))
+        self.ln_final = LayerNorm(transformer_W)
+        
+        self.text_proj = nn.Parameter(torch.empty(transformer_W, embed_dim))
+        self.logit_scale = nn.Parameter(torch.ones([]) * np.log(1 / 0.07))
+
+        self.initialize_params()
+
+    def initialize_params(self):
+        nn.init.normal_(self.token_emb.weight, std=0.02)
+        nn.init.normal_(self.pos_emb, std=0.01)
+
+        if isinstance(self.visual, ModifiedResNet):
+            if self.visual.attnpool is not None:
+                std = self.visual.attnpool.c_proj.in_features ** -0.5
+                nn.init.normal_(self.visual.attnpool.q_proj.weight, std=std)
+                nn.init.normal_(self.visual.attnpool.k_proj.weight, std=std)
+                nn.init.normal_(self.visual.attnpool.v_proj.weight, std=std)
+                nn.init.normal_(self.visual.attnpool.c_proj.weight, std=std)
+
+            for resnet_block in [self.visual.layer1, self.visual.layer2, self.visual.layer3, self.visual.layer4]:
+                for name, param in resnet_block.named_parameters():
+                    if name.endswith("bn3.weight"):
+                        nn.init.zeros_(param)
+        
+        proj_std = (self.transformer.width ** -0.5) * ((2 * self.transformer.layers) ** -0.5)
+        attn_std = self.transformer.width ** -0.5
+        fc_std = (2 * self.transformer.width) ** -0.5
+        for block in self.transformer.resblocks:
+            nn.init.normal_(block.attn.in_proj_weight, std = attn_std)
+            nn.init.normal_(block.attn.out_proj.weight, std = proj_std)
+            nn.init.normal_(block.mlp.c_fc.weight, std = fc_std)
+            nn.init.normal_(block.mlp.c_proj.weight, std = proj_std)
+            
+        if self.text_proj is not None:
+            nn.init.normal_(self.text_proj, std=self.transformer.width ** -0.5)
+
+    def build_attn_mask(self):
+        """
+        Create Casual Attention Mask with full attention b/w visual tokens 
+        """
+        mask = torch.empty(self.context_length, self.context_length)
+        mask.fill_(float("-inf"))
+        mask.triu_(1) # zero out the lower diagonal
+
+        return mask
+
+    @property
+    def dtype(self):
+        return self.visual.conv1.weight.dtype
+    
+    def encode_img(self, img):
+        return self.visual(img.type(self.dtype))
+
+    def encode_txt(self, txt):
+        x = self.token_emb(txt).type(self.dtype) # [batch_size, n_ctx, d_model]
+        x = x + self.pos_emb.type(self.dtype)
+        x = x.permute(1, 0, 2) # NLD -> LND
+        x = self.transformer(x)
+        x = x.permute(1, 0, 2) # LND -> NLD
+        x = self.ln_final(x).type(self.dtype)
+
+        # [batch_size, n_ctx, transformer_width]
+        # features from EOT embedding (eot_token is highest number in each sequence)
+        x = x[torch.arange(x.shape[0]), txt.argmax(dim=-1)] @ self.text_proj
+
+        return x 
+    
+    def forward(self, image, txt):
+        img_features = self.encode_img(image)
+        txt_features = self.encode_txt(txt)
+
+        # Normalized Features
+        img_features = img_features / img_features.norm(dim=1, keepdim=True)
+        txt_features = txt_features / txt_features.norm(dim=1, keepdim=True)
+
+        # Cosine Similarity as logits
+        logit_scale = self.logit_scale.exp()
+        logits_per_image = logit_scale * img_features @ txt_features.T()
+        logits_per_txt = logits_per_image.T()
+
+        return logits_per_image, logits_per_txt # [global_batch_size, global_batch_size]
+    
+
+def convert_weights(model: nn.Module):
+    """ Convert model parameters to FP16 """
+    def _convert_weights_fp16(l):
+        if isinstance(l, (nn.Conv1d, nn.Conv2d, nn.Linear)):
+            l.weight.data = l.weight.data.half()
+            if l.bias is not None:
+                l.bias.data = l.bias.data.half()
+
+        if isinstance(l, nn.MultiheadAttention):
+            for attr in [*[f"{s}_proj_weight" for s in ["in", "q", "k", "v"]], "in_proj_bias", "bias_k", "bias_v"]:
+                tensor = getattr(l, attr)
+                if tensor is not None:
+                    tensor.data = tensor.data.half()
+
+        for name in ['text_proj', 'proj']:
+            if hasattr(l, name):
+                attr = getattr(l, name)
+                if attr is not None:
+                    attr.data = attr.data.half()
+    
+    model.apply(_convert_weights_fp16)
+
+def build_model(state_dict: dict):
+    vit = 'visual.proj' in state_dict
+
+    if vit:
+        vision_width = state_dict['visual.conv1.weight'].shape[0]
+        vision_layers = len([k for k in state_dict.keys() if k.startswith("visual.") and k.endswith(".attn.in_proj_weight")])
+        vision_patch_size = state_dict["visual.conv1.weight"].shape[-1]
+        grid_size = round((state_dict["visual.pos_emb"].shape[0] - 1) ** 0.5)
+        image_res = vision_patch_size * grid_size
+    else:
+        counts: list = [len(set(k.split(".")[2] for k in state_dict if k.startswith(f"visual.layer{b}"))) for b in [1, 2, 3, 4]]
+        vision_layers = tuple(counts)
+        vision_width = state_dict["visual.layer1.0.conv1.weight"].shape[0]
+        output_width = round((state_dict["visual.attnpool.pos_emb"].shape[0] - 1) ** 0.5)
+        vision_patch_size = None
+        assert output_width ** 2 + 1 == state_dict["visual.attnpool.pos_emb"].shape[0]
+        image_res = output_width * 32
+
+    embed_dim = state_dict['text_proj'].shape[1]
+    context_length = state_dict['pos_emb'].shape[0]
+    vocab_size = state_dict['token_emb'].shape[0]
+    transformer_width = state_dict['ln_final.weight'].shape[0]
+    transformer_heads = transformer_width // 64
+    transformer_layers = len(set(k.split(".")[2] for k in state_dict if k.startswith("transformer.resblocks")))
+
+    model = CLIP(
+        embed_dim = embed_dim,
+        image_res = image_res, vision_layers = vision_layers, vision_width = vision_width, vision_patch_size = vision_patch_size,
+        context_length = context_length, vocab_size = vocab_size, transformer_W = transformer_width, 
+        transformer_heads = transformer_heads, transformer_layers = transformer_layers
+    )
+
+    for key in ['input_res', 'context_length', 'vocab_size']:
+        if key in state_dict:
+            del state_dict[key]
+    
+    convert_weights(model)
+    model.load_state_dict(state_dict)
+
+    return model.eval()
